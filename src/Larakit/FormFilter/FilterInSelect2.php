e98355682a6e1982dfdda96f5981204622e0bfe8
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-21 20:46:20 +0500

    fixed

--- src/Larakit/FormFilter/FilterInSelect2.php
+++ src/Larakit/FormFilter/FilterInSelect2.php
@@ -16,13 +16,14 @@ class FilterInSelect2 extends Filter {
 
     function element(\HTML_QuickForm2_Container $form) {
         $form->putSelect2Twbs($this->form_field)
+            ->setMultiple(true)
             ->setLabel($this->label)
             ->loadOptions($this->options);
     }
 
     function query($model) {
         if($this->value) {
-            $values = array_map('intval', $this->value);
+            $values = array_map('intval', (array)$this->value);
             if($this->relation) {
                 $model->whereHas($this->relation, function ($query) use ($values) {
                     $query->whereIn($this->db_field, $values);
