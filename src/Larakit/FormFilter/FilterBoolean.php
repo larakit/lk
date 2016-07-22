74ab3136b2d16fb4955821b92159c14c0b1500e3
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-22 18:07:51 +0500

    fixed

--- src/Larakit/FormFilter/FilterBoolean.php
+++ src/Larakit/FormFilter/FilterBoolean.php
@@ -14,24 +14,66 @@ use Larakit\QuickForm\LaraForm;
 
 class FilterBoolean extends Filter {
 
+    protected $no  = 'Нет';
+    protected $yes = 'Да';
+    protected $all = 'Все';
+
+    /**
+     * @param string $no
+     *
+     * @return FilterBoolean;
+     */
+    public function setNo($no) {
+        $this->no = $no;
+
+        return $this;
+    }
+
+    /**
+     * @param string $yes
+     *
+     * @return FilterBoolean;
+     */
+    public function setYes($yes) {
+        $this->yes = $yes;
+
+        return $this;
+    }
+
+    /**
+     * @param string $all
+     *
+     * @return FilterBoolean;
+     */
+    public function setAll($all) {
+        $this->all = $all;
+
+        return $this;
+    }
+
     function element(\HTML_QuickForm2_Container $form) {
         $this->options = [
-            -1 => 'Нет',
-            0 => 'ALL',
-            1 => 'YES',
+            0  => $this->all,
+            -1 => $this->no,
+            1  => $this->yes,
         ];
-        $form->put($this->form_field, $this->options)->setLabel($this->label);
+        $form->putGroupRadioButtonTwbs($this->form_field, $this->options)->setLabel($this->label);
+        if(!$this->value) {
+            $form->getForm()->initValues([
+                $this->form_field => 0,
+            ]);
+        }
     }
 
     function query($model) {
-        $v = (int)$this->value;
-        if(0!=$v){
+        $v = (int) $this->value;
+        if(0 != $v) {
             if($this->relation) {
                 $model->whereHas($this->relation, function ($query) use ($v) {
-                    $query->where($this->db_field, '=',(1==$v));
+                    $query->where($this->db_field, '=', (1 == $v));
                 });
             } else {
-                $model->where($this->db_field, '=',(1==$v));
+                $model->where($this->db_field, '=', (1 == $v));
             }
         }
     }
--- src/Larakit/FormFilter/FilterIn.php
+++ src/Larakit/FormFilter/FilterIn.php
@@ -15,23 +15,22 @@ use Larakit\QuickForm\LaraForm;
 class FilterIn extends Filter {
 
     function element(\HTML_QuickForm2_Container $form) {
-        function element(\HTML_QuickForm2_Container $form) {
-            $cnt = count($this->options);
-            switch(true){
-                case ($cnt<=5):
-                    $form->putGroupCheckboxButtonTwbs($this->form_field, $this->options)->setLabel($this->label);
-                    break;
-                case ($cnt<=15):
-                    $form->putGroupCheckboxTwbs($this->form_field, $this->options, false)->setLabel($this->label);
-                    break;
-                default:
-                    $form->putSelect2Twbs($this->form_field)
-                        ->setMultiple(true)
-                        ->setLabel($this->label)
-                        ->loadOptions($this->options);
-                    break;
-            }
-        }    }
+        $cnt = count($this->options);
+        switch(true) {
+            case ($cnt <= 5):
+                $form->putGroupCheckboxButtonTwbs($this->form_field, $this->options)->setLabel($this->label);
+                break;
+            case ($cnt <= 15):
+                $form->putGroupCheckboxTwbs($this->form_field, $this->options, false)->setLabel($this->label);
+                break;
+            default:
+                $form->putSelect2Twbs($this->form_field)
+                    ->setMultiple(true)
+                    ->setLabel($this->label)
+                    ->loadOptions($this->options);
+                break;
+        }
+    }
 
     function query($model) {
         if($this->value) {
